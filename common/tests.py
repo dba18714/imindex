4a587998e4b19bb633088dcc814e76d8cdc72d9b
sv <dba18714@gmail.com>
2023-12-17 16:15:45 +0800

    -4

--- common/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.core.cache import cache
-
-
-class RedisTestCase(TestCase):
-    def test_redis_connection(self):
-        cache.set('test_key', 'test_value', timeout=60)
-        value = cache.get('test_key')
-        self.assertEqual(value, 'test_value')
--- ims/components/detail.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from django_unicorn.components import UnicornView
-from django.core.paginator import Paginator
-from ..models import Link
-
-
-class DetailView(UnicornView):
-    pass
