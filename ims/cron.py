228623011283641a6262dfc2853ec1a8682fb2e0
sv <dba18714@gmail.com>
2023-12-21 14:55:43 +0800

    -99

--- ims/cron.py
+++ ims/cron.py
@@ -10,11 +10,12 @@ from ims.models import Link
 # 通常，你会在服务器上设置一个定期运行 runcrons 命令的计划任务（例如，使用 crontab）。
 
 class Runspider(CronJobBase):
-    schedule = Schedule(run_at_times=['00:00'])
+    schedule = Schedule(run_at_times=['03:00', '15:00'])
     code = 'ims.cron.Runspider'  # 一个唯一的代码
+    allow_parallel_runs = False  # 防止任务重叠，如果上一个任务实例仍在运行，新的实例将不会启动
 
     def do(self):
-        logger.info("CronJob:Runspider start ---------------------------------------")
+        logger.info("CronJob:Runspider start --------------------------------------")
         call_command('runspider')
 
 
