34955f9c3746489a4a2348a2083800b6f45a3ddb
sv <dba18714@gmail.com>
2023-12-24 00:06:07 +0800

    19

--- ims/tasks.py
+++ ims/tasks.py
@@ -16,14 +16,15 @@ def verify_telegram(link_id):
 
 @shared_task
 def spider_for_tgcng_com():
-    from ims.models import Link
-    num_a = 1.0
-    num_b = 20.0
-    for word in get_words():
-        ids = get_info_ids(word)
-        for info_id in ids:
-            url = get_telegram_url(info_id)
-            if url:
-                link, created = Link.objects.get_or_create(url=url)
-            time.sleep(random.uniform(num_a, num_b))
+    pass
+    # from ims.models import Link
+    # num_a = 1.0
+    # num_b = 20.0
+    # for word in get_words():
+    #     ids = get_info_ids(word)
+    #     for info_id in ids:
+    #         url = get_telegram_url(info_id)
+    #         if url:
+    #             link, created = Link.objects.get_or_create(url=url)
+    #         time.sleep(random.uniform(num_a, num_b))
 
