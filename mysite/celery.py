73100caf178989b0037c030e475c916f27307c1e
sv <dba18714@gmail.com>
2023-12-25 18:51:41 +0800

    41

--- mysite/celery.py
+++ mysite/celery.py
@@ -18,17 +18,17 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # 自动从所有已注册的 Django app 加载任务
 app.autodiscover_tasks()
 
-# # 创建日志记录器
-# logger = logging.getLogger('celery')
-# logger.setLevel(logging.INFO)
-#
-# # 创建处理程序
-# log_file_path = os.path.join(settings.BASE_DIR, 'logs/celery.log')
-# handler = RotatingFileHandler(log_file_path, maxBytes=10000, backupCount=3)
-#
-# # 创建并设置格式化器
-# formatter = logging.Formatter('[%(asctime)s: %(levelname)s/%(processName)s] %(message)s')
-# handler.setFormatter(formatter)
-#
-# # 添加处理程序到记录器
-# logger.addHandler(handler)
+# 创建日志记录器
+logger = logging.getLogger('celery')
+logger.setLevel(logging.INFO)
+
+# 创建处理程序
+log_file_path = os.path.join(settings.BASE_DIR, 'logs/celery.log')
+handler = RotatingFileHandler(log_file_path, maxBytes=10000, backupCount=3)
+
+# 创建并设置格式化器
+formatter = logging.Formatter('[%(asctime)s: %(levelname)s/%(processName)s] %(message)s')
+handler.setFormatter(formatter)
+
+# 添加处理程序到记录器
+logger.addHandler(handler)
