21fa5d92df392d37407d7103124efa293b017f8f
sv <dba18714@gmail.com>
2023-12-26 05:30:45 +0800

    55

--- ims/services.py
+++ ims/services.py
@@ -16,6 +16,7 @@ def verify_telegram(link_id):
     try:
         link = Link.objects.get(id=link_id)
         url = link.url
+        logger.info(f"verify_telegram URL：{url} -----------------")
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
