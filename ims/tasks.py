e08355d687aba57f7c2d507e5590d61d94a2edce
sv <dba18714@gmail.com>
2023-12-27 17:00:36 +0800

    71

--- ims/tasks.py
+++ ims/tasks.py
@@ -4,6 +4,7 @@ import time
 
 from celery import shared_task
 from django.apps import apps
+from django.db.models import Q
 from django.forms import model_to_dict
 
 # from django.utils.autoreload import logger
@@ -15,6 +16,15 @@ from ims import services
 logger = logging.getLogger('django')
 
 
+def get_or_create_link(url):
+    from ims.models import Link
+    link, created = Link.objects.get_or_create(
+        Q(url__iexact=url),
+        defaults={'url': url}  # 如果需要创建新对象，则使用这些默认值
+    )
+    return link, created
+
+
 @shared_task
 def verify_telegram(link_id):
     logger.info("task verify_telegram start -----------------")
@@ -23,7 +33,6 @@ def verify_telegram(link_id):
 
 @shared_task
 def spider_for_tgcng_com():
-    from ims.models import Link
     logger.info("spider_for_tgcng_com start -----------------")
     num_a = 1.0
     num_b = 10.0
@@ -32,13 +41,12 @@ def spider_for_tgcng_com():
     for info_id in ids[:50]:
         telegram_url = get_telegram_url(info_id)
         if telegram_url:
-            link, created = Link.objects.get_or_create(url=telegram_url)
+            get_or_create_link(url=telegram_url)
         time.sleep(random.uniform(num_a, num_b))
 
 
 @shared_task
 def spider_for_tgsou_me():
-    from ims.models import Link
     logger.info("spider_for_tgsou_me start -----------------")
     num_a = 1.0
     num_b = 10.0
@@ -46,13 +54,12 @@ def spider_for_tgsou_me():
     logger.info(f"telegram_urls: {telegram_urls}")
     for telegram_url in telegram_urls:
         if telegram_url:
-            link, created = Link.objects.get_or_create(url=telegram_url)
+            get_or_create_link(url=telegram_url)
             time.sleep(random.uniform(num_a, num_b))
 
 
 @shared_task
 def spider_for_tgsou_me_from_html():
-    from ims.models import Link
     logger.info("spider_for_tgsou_me_from_html start -----------------")
     num_a = 1.0
     num_b = 10.0
@@ -60,5 +67,5 @@ def spider_for_tgsou_me_from_html():
     logger.info(f"telegram_urls: {telegram_urls}")
     for telegram_url in telegram_urls:
         if telegram_url:
-            link, created = Link.objects.get_or_create(url=telegram_url)
+            get_or_create_link(url=telegram_url)
             time.sleep(random.uniform(num_a, num_b))
