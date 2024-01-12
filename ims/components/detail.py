ae31875683b6374c7c81769dd7194c728bd3574f
sv <dba18714@gmail.com>
2024-01-12 16:21:06 +0800

    89

--- ims/components/detail.py
+++ ims/components/detail.py
@@ -1,5 +1,6 @@
 import time
 
+from django.db.models.expressions import RawSQL
 from django_unicorn.components import UnicornView
 from django.core.paginator import Paginator
 from ..models import Link
@@ -7,3 +8,6 @@ from ..models import Link
 
 class DetailView(UnicornView):
     hot_links = Link.objects.verified_and_valid().order_by('-member_count')[:5]
+    random_links = Link.objects.verified_and_valid().annotate(
+        random_order=RawSQL('RANDOM()', [])
+    ).order_by('random_order')[:5]
