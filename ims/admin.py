70bba8159cb6db48ee942827cd9d27ca94a49cba
dba18714 <dba18714@gmail.com>
2024-03-20 16:53:16 +0800

    Update list_display and list_filter in Admin class

--- ims/admin.py
+++ ims/admin.py
@@ -22,8 +22,8 @@ action_verify_telegram.short_description = '验证 Telegram URL'
 
 class Admin(admin.ModelAdmin):
     list_per_page = 30
-    list_display = ["id", "show_name", "member_count", "show_url", "verified_at", "created_at"]
-    list_filter = ["is_by_user", "is_valid", "verified_at"]
+    list_display = ["id", "show_name", "member_count", "show_url", "verified_start_at", "verified_at", "created_at"]
+    list_filter = ["is_by_user", "is_valid", "verified_start_at", "verified_at"]
     search_fields = ["id", "uuid", "name", "description", "member_count", "url"]
     actions = [action_verify_telegram]
 
