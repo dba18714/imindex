f363f036999fceea59f4fad66f83c57df444d48d
dba18714 <dba18714@gmail.com>
2024-03-23 17:09:24 +0800

    Update dependencies and remove commented code

--- _for_deploy/start.sh
+++ _for_deploy/start.sh
@@ -3,7 +3,7 @@
 python manage.py compilemessages &&
 python manage.py collectstatic --noinput &&
 _for_deploy/wait-for-db.sh db &&
-service cron start &&
+# service cron start &&
 python manage.py migrate &&
 python manage.py imindex_install &&
 python manage.py runserver 0.0.0.0:8000
