e83aa0ac90b4971700336c3f2f84b27586a4d01b
dba18714 <dba18714@gmail.com>
2024-04-26 00:37:10 +0800

    Update web scraper to use database for word retrieval

--- crawler/management/commands/runspider.py
+++ crawler/management/commands/runspider.py
@@ -5,21 +5,33 @@ from django.core.management.base import BaseCommand
 
 import common.utils
 from crawler.spiders.spider import scrape_with_xpath
-from crawler.tgcng_com import get_telegram_url, get_words, get_info_ids
+from crawler.tgcng_com import get_telegram_url, get_words, get_info_ids, get_words_by_db
 from ims.models import Link
+from ims.services import get_or_create_link
 
 
 class Command(BaseCommand):
     help = 'Run the web scraper'
 
-    # def handle(self, *args, **kwargs):
-    #     num_a = 1.0
-    #     num_b = 20.0
-    #     for word in get_words():
-    #         ids = get_info_ids(word)
-    #         for info_id in ids:
-    #             url = get_telegram_url(info_id)
-    #             if url:
-    #                 link, created = Link.objects.get_or_create(url=url)
-    #             time.sleep(random.uniform(num_a, num_b))
-    #
+    def handle(self, *args, **kwargs):
+        num_a = 0.2
+        num_b = 1.0
+        # for word in get_words():
+        # urls = [
+        #     'https://www.tgcng.com/tags.php',
+        #     'https://github.com/itgoyo/TelegramGroup',
+        #     'https://github.com/jackhawks/rectg',
+        #     'https://s.weibo.com/top/summary',
+        # ]
+        # url = random.choice(urls)
+        # words = get_words(url=url)
+        words = get_words_by_db()
+        for word in words[:10]:
+            ids = get_info_ids(word)
+            for info_id in ids[:14]:
+                telegram_url = get_telegram_url(info_id)
+                if telegram_url:
+                    get_or_create_link(url=telegram_url)
+                    time.sleep(random.uniform(num_a, num_b))
+            time.sleep(random.uniform(num_a, num_b))
+    
