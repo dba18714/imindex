bc73b981bfae39c808fe4b4f00604a52a5b6152b
dba18714 <dba18714@gmail.com>
2024-04-26 16:02:54 +0800

    Refactor word iteration limit and add logging statement in runspider.py

--- crawler/management/commands/runspider.py
+++ crawler/management/commands/runspider.py
@@ -31,9 +31,9 @@ class Command(BaseCommand):
         # url = random.choice(urls)
         # words = get_words(url=url)
         words = get_words_by_db()
-        for word in words[:10]:
+        for word in words[:20]:
             ids = get_info_ids(word)
-            for info_id in ids[:10]:
+            for info_id in ids[:20]:
                 telegram_url = get_telegram_url(info_id)
                 if telegram_url:
                     get_or_create_link(url=telegram_url)
