8c4b608531290e28d9e9107855d754f4d689b4a1
dba18714 <dba18714@gmail.com>
2024-05-23 12:26:57 +0800

    Refactor cron.py to remove unnecessary spider_for_tgcng_com.delay() calls and update get_links() call to retrieve 25 links instead of 50

--- ims/cron.py
+++ ims/cron.py
@@ -28,7 +28,7 @@ class Runspider(CronJobBase):
         # call_command('runspider')
         tasks.spider_for_tgcng_com.delay()
         tasks.spider_for_tgcng_com.delay()
-        tasks.spider_for_tgcng_com.delay()
+        # tasks.spider_for_tgcng_com.delay()
         # tasks.spider_for_tgsou_me_from_html.delay()  # 已被cf拦截请求
         # tasks.spider_for_tgsou_me.delay()  # 已被cf拦截请求
 
@@ -45,7 +45,7 @@ class VerifyTelegram(CronJobBase):
 
     def do(self):
         logger.info("CronJob:VerifyTelegram start -----------------")
-        links = get_links(50)
+        links = get_links(25)
         for link in links:
             link_dict = model_to_dict(link)
             link_dict['created_at'] = date_format(link.created_at.astimezone(), format='DATETIME_FORMAT') if link.created_at else 'N/A'
